71194789efefab494c8514b805acaf7d73f50685
peterdeepsix <52168549+peterdeepsix@users.noreply.github.com>
2019-11-21 14:04:45 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,12 @@
+# Install Docker Engine
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+
+# Add the Cloud SDK distribution URI as a package source
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update -y && sudo apt-get install google-cloud-sdk -y
