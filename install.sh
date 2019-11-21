ee39b0af8da7cdcf4e05894c01c43c2d698be633
peterdeepsix <52168549+peterdeepsix@users.noreply.github.com>
2019-11-21 13:59:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,2 @@
-sudo apt-get update
-
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg2 \
-    software-properties-common
-    
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-   
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
